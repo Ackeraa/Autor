fb23aa1bcc9b4994b490cf9fb76f76bdf36d1693
Ackeraa <acker2880@gmail.com>
2022-09-18 18:29:06 +0800

    fixed diff

--- diff.sh
+++ diff.sh
@@ -5,25 +5,23 @@ diff2f=./diff2html.sh
 GITHUB_USERNAME=Ackeraa
 
 today=$(date -d $(TZ=UTC-8 date +%Y-%m-%d) +"%Y-%m-%dT%H:%M:%S%z")
-repos=($(curl --silent https://api.github.com/users/$GITHUB_USERNAME/repos?per_page=100? \
+repos=($(curl --silent "https://api.github.com/users/$GITHUB_USERNAME/repos?per_page=100" \
 	| grep -w clone_url | sed 's/,/\n/g' | gawk '{ print $2 }' \
 	| gawk -F/ '{ print $5 }' | sed 's/"//; s/\.git$//'))
 
 for repo in ${repos[6]}
 do
-    commits=$(curl --silent https://api.github.com/repos/$GITHUB_USERNAME/$repo/commits?since=$today&per_page=100)
-    shas=($(echo "$commits" | sed -n '/"sha"/p' | gawk '/sha/{print $2}' | sed 's/,\|"//g'))
-    dates=($(echo "$commits" | sed -n '/"date"/p' | uniq | gawk '{print $2}' | sed 's/"//g'))
-    n=${#dates[*]}
-    echo $n
-    echo "${shas[*]}"
-    if [ $n -gt 0 ]; # I commited today
+    shas=($(curl --silent "https://api.github.com/repos/$GITHUB_USERNAME/$repo/commits?since=$today&per_page=100" \
+    	| sed -n '/"sha"/p' \
+	| gawk '/sha/{print $2}' \
+	| sed 's/,\|"//g'))
+
+    if [ ${#shas[*]} -gt 0 ]; # I commited today
     then
-	last=$[ $n * 3 - 3 ] # find today's first commit's parent commit
-	osha=${shas[$last]} 
+	osha=${shas[-1]} 
 	nsha=${shas[0]}
 	echo "<h1>$repo</h1>"
 	echo "https://github.com/$GITHUB_USERNAME/$repo/compare/$osha...$nsha.diff" 
-	curl --silent "https://github.com/$GITHUB_USERNAME/$repo/compare/$osha...$nsha.diff" | $diff2f > /dev/null
+	curl --silent "https://github.com/$GITHUB_USERNAME/$repo/compare/$osha...$nsha.diff" | $diff2f 
     fi
 done
