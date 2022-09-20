be045bb172fd59d77aa8714988bb848d09c3dbc6
Ackeraa <acker2880@gmail.com>
2022-09-20 22:33:08 +0800

    fixed name

--- diff.sh
+++ diff.sh
@@ -4,13 +4,13 @@
 diff2f=./diff2html.sh
 
 today=$(date -d $(TZ=UTC-8 date +%Y-%m-%d) +"%Y-%m-%dT%H:%M:%S%z")
-repos=($(curl --silent "https://api.github.com/users/$GITHAB_USERNAME/repos?per_page=100" \
+repos=($(curl --silent "https://api.github.com/users/$GITHUB_USERNAME/repos?per_page=100" \
 	| grep -w clone_url | sed 's/,/\n/g' | gawk '{ print $2 }' \
 	| gawk -F/ '{ print $5 }' | sed 's/"//; s/\.git$//'))
 
 for repo in ${repos[*]}
 do
-    shas=($(curl --silent "https://api.github.com/repos/$GITHAB_USERNAME/$repo/commits?since=$today&per_page=100" \
+    shas=($(curl --silent "https://api.github.com/repos/$GITHUB_USERNAME/$repo/commits?since=$today&per_page=100" \
     	| sed -n '/"sha"/p' \
 	| gawk '/sha/{print $2}' \
 	| sed 's/,\|"//g'))
@@ -21,7 +21,7 @@ do
 	osha=${shas[-1]} 
 	nsha=${shas[0]}
 	echo "<h1>$repo</h1>"
-	echo "https://github.com/$GITHAB_USERNAME/$repo/compare/$osha...$nsha.diff" 
-	curl --silent "https://github.com/$GITHAB_USERNAME/$repo/compare/$osha...$nsha.diff" | $diff2f 
+	echo "https://github.com/$GITHUB_USERNAME/$repo/compare/$osha...$nsha.diff" 
+	curl --silent "https://github.com/$GITHUB_USERNAME/$repo/compare/$osha...$nsha.diff" | $diff2f 
     fi
 done
